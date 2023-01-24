98dfedd398f1dc4a3288c0209772b82b8f751781
Roshane Miller <millerroshane44@gmail.com>
2023-01-24 16:40:56 -0500

    reverted changes

--- hooks/useRenderList.js
+++ hooks/useRenderList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { nanoid } from "nanoid"
 import Link from "next/link"
 
@@ -10,40 +10,44 @@ export default function useRenderList(
   class2Desk = null,
   pathname = null
 ) {
-  console.log(array)
-  const [elements, setElements] = useState(
-    array.map((item) => {
-      return item.toLowerCase() === "resume" ? (
-        <li key={nanoid()}>
-          <Link
-            className={size === "desktop" ? class1Desk : classMob}
-            href="https://resume.io/r/bwFS0PXmq"
-            target={"_blank"}
-          >
-            {item}
-          </Link>
-        </li>
-      ) : pathname !== "/" ? (
-        <li key={nanoid()}>
-          <Link
-            className={size === "desktop" ? class2Desk : classMob}
-            href={`/#${item}`}
-          >
-            {item}
-          </Link>
-        </li>
-      ) : (
-        <li key={nanoid()}>
-          <Link
-            className={size === "desktop" ? class1Desk : classMob}
-            href={`#${item}`}
-          >
-            {item}
-          </Link>
-        </li>
-      )
-    })
-  )
+  const [elements, setElements] = useState()
+  useEffect(() => {
+    setElements(
+      array.map((item) => {
+        console.log(item)
+        console.log(pathname !== "/")
+        return item.toLowerCase() === "resume" ? (
+          <li key={nanoid()}>
+            <Link
+              className={size === "desktop" ? class1Desk : classMob}
+              href="https://resume.io/r/bwFS0PXmq"
+              target={"_blank"}
+            >
+              {item}
+            </Link>
+          </li>
+        ) : pathname !== "/" ? (
+          <li key={nanoid()}>
+            <Link
+              className={size === "desktop" ? class2Desk : classMob}
+              href={`/#${item}`}
+            >
+              {item}
+            </Link>
+          </li>
+        ) : (
+          <li key={nanoid()}>
+            <Link
+              className={size === "desktop" ? class1Desk : classMob}
+              href={`#${item}`}
+            >
+              {item}
+            </Link>
+          </li>
+        )
+      })
+    )
+  }, [])
 
   return [elements]
 }
